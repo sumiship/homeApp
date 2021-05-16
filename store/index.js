9f773a3a585d18f814d8d4021345b47c1acf3c6d
sumiship <osumi.k.osumi.k@gmail.com>
2021-05-16 17:46:49 +0900

    hitAndBlow URL gitHubPages -> netlify

--- store/index.js
+++ store/index.js
@@ -3,38 +3,38 @@ export const state = () => ({
     post1: {
       id: 1,
       name: "hitAndBlow",
-      url: "https://sumiship.github.io/hitAndBlow/",
-      image_id: "post1.png",
+      url: "https://ecstatic-yonath-df241f.netlify.app/",
+      image_id: "post1.png"
     },
     post2: {
       id: 2,
       name: "yonmokunarabe",
       url: "https://sumiship.github.io/yonnmokunarabe/",
-      image_id: "post2.png",
+      image_id: "post2.png"
     },
     post3: {
       id: 3,
       name: "wordQuiz",
       url: "https://sumiship.github.io/wordQuiz/",
-      image_id: "post3.png",
+      image_id: "post3.png"
     },
     post4: {
       id: 4,
       name: "地雷処理FPS",
       url: "https://sumiship.github.io/fpsGame/",
-      image_id: "post4.png",
+      image_id: "post4.png"
     },
     post5: {
       id: 5,
       name: "脳トレアクション",
       url: "https://sumiship.github.io/ActionGame/",
-      image_id: "post5.png",
+      image_id: "post5.png"
     },
     post6: {
       id: 6,
       name: "走り記録する",
       url: "https://sumiship.github.io/rundatas/",
-      image_id: "post6.png",
-    },
+      image_id: "post6.png"
+    }
   }
-})
+});
